97d767e3feb3b0e832224745d2893a23af28bf9f
butlerx <butlerx@notthe.cloud>
2020-07-19 08:36:25 +0100

    add command to check id a user is free

--- src/free.py
+++ src/free.py
@@ -1,14 +1,23 @@
 """functions for checking if a username is free"""
 
+from bonsai import LDAPClient
+from bonsai.ldapconnection import LDAPConnection
+
+from .file import read_file
+
 
 def check_free_cli(args):
     """check if a username is free in ldap cli interface"""
-    if check_free(args.username):
-        print(f"{args.username} is free")
-    else:
-        print(f"{args.username} is taken")
+    client = LDAPClient(f"ldap://{args.host}")
+    client.set_credentials("SIMPLE", user=args.user, password=read_file(args.password))
+    with client.connect() as conn:
+        if check_free(conn, args.username):
+            print(f"{args.username} is free")
+        else:
+            print(f"{args.username} is taken")
 
 
-def check_free(username: str) -> bool:
+def check_free(conn: LDAPConnection, username: str) -> bool:
     """check if a username is free in ldap"""
-    return True
+    res = conn.search("ou=accounts,o=redbrick", 2, f"(uid={username})")
+    return bool(res)
