37fbdd7155e49bad533484f23fcf3612f15c560b
butlerx <butlerx@notthe.cloud>
2020-07-22 10:25:40 +0100

    i heard you like list comprehension so i put some list comprehension in your list comprehension

--- rbldap/commands/generate.py
+++ rbldap/commands/generate.py
@@ -1,6 +1,6 @@
 """generate vhost config"""
-import os.path
-import sys
+from os import path
+from typing import Optional
 
 from bonsai import LDAPClient
 
@@ -26,6 +26,27 @@ def user2nix(uid: str, home: str, gid: str) -> str:
   }}"""
 
 
+def ldap2nix(user: dict, webtree: str) -> Optional[str]:
+    """
+    Convert ldap entry to nix config
+
+    Args:
+        user: raw response from ldap client for user
+        webtree: root webtree path
+    Returns:
+        Returns String containing a nix object for inserting in a nix array
+    """
+    uid = user["uid"][0]
+    home = user["homeDirectory"][0]
+    gid = gid_to_usertype(user["gidNumber"][0])
+
+    if uid and home and gid:
+        if path.exists(f"{webtree}/{uid[0]}/{uid}") or "/var/lib" in home:
+            return user2nix(uid=uid, home=home, gid=gid)
+        print(f"Skipping {uid}: missing webtree")
+    return None
+
+
 async def generate(rb_client: LDAPClient, output: str, *, webtree: str = "/webtree"):
     """
     Generate nix config for user vhosts based off ldap
@@ -35,9 +56,6 @@ async def generate(rb_client: LDAPClient, output: str, *, webtree: str = "/webtr
         output: path to write generate config too
         webtree: Root folder of webtree
     """
-    num_users = 0
-    nix = []
-
     async with rb_client.connect(is_async=True) as conn:
         res = await conn.search(
             "ou=accounts,o=redbrick",
@@ -45,19 +63,11 @@ async def generate(rb_client: LDAPClient, output: str, *, webtree: str = "/webtr
             "(objectclass=posixAccount)",
             attrlist=["uid", "homeDirectory", "gidNumber"],
         )
-    for user in res:
-        uid = user["uid"][0]
-        home = user["homeDirectory"][0]
-        gid = gid_to_usertype(user["gidNumber"][0])
+    nix = [
+        conf for conf in [ldap2nix(user, webtree) for user in res] if conf is not None
+    ]
 
-        if uid and home and gid:
-            if os.path.exists(f"{webtree}/{uid[0]}/{uid}") or "/var/lib" in home:
-                nix.append(user2nix(uid=uid, home=home, gid=gid))
-                num_users += 1
-            else:
-                print(f"Skipping {uid}: missing webtree")
-
-    print(f"Generated nix config for {str(num_users)} users")
+    print(f"Generated nix config for {str(len(nix))} users")
     nix_conf = "\n".join(nix)
     with open(output, "w+") as f:
         f.write(f"[\n{nix_conf}\n]")
